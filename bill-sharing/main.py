3eb8ca0418d6ee0b922e9fa1e6f108162abfd109
bharath <bharathh1817@gmail.com>
2026-04-20 22:44:24 +0530

    implement Flatmate class with initialization and example instances

--- bill-sharing/main.py
+++ bill-sharing/main.py
@@ -1,1 +1,10 @@
+class Flatmate:
+  def __init__(self,name,days_in_the_house):
+    self.name = name
+    self.days_in_the_house = days_in_the_house
 
+f1 = Flatmate("Bharath",11)
+f2 = Flatmate("John",23)
+
+print(f1.name)
+print(f2.days_in_the_house)
