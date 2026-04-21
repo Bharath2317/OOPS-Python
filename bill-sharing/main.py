0b58cc322ed067e6fd33578827c0e43ec72bddfa
bharath <bharathh1817@gmail.com>
2026-04-21 19:01:09 +0530

    add Bill class and update Flatmate pays method for bill sharing calculation

--- bill-sharing/main.py
+++ bill-sharing/main.py
@@ -1,10 +1,23 @@
+class Bill:
+  def __init__(self,amount,period):
+    self.amount = amount
+    self.period = period
+
+
 class Flatmate:
   def __init__(self,name,days_in_the_house):
     self.name = name
     self.days_in_the_house = days_in_the_house
 
-f1 = Flatmate("Bharath",11)
-f2 = Flatmate("John",23)
+  def pays(self,bill,flatmate2):
+    day_bill = self.days_in_the_house / (self.days_in_the_house+flatmate2.days_in_the_house)
+    to_pay = bill.amount * day_bill
+    return to_pay
+  
+the_bill = Bill(180,"March 30")
+flatmate1 = Flatmate("john",25)
+flatmate2 = Flatmate("Arjun",28)
 
-print(f1.name)
-print(f2.days_in_the_house)
+print(flatmate1.pays(the_bill,flatmate2))
+print(flatmate2.pays(the_bill,flatmate1))
+
